bb4cdf301f5d16f5c085aeef51dc7aee077ddabd
Vedant <ved.k96@gmail.com>
2022-04-27 15:30:57 +0530

    Assignment 17

---
+++ April/Assignment 17/index.js
@@ -0,0 +1,12 @@
+var img = document.createElement("img");
+
+img.src = "https://www.uspto.gov/sites/default/files/styles/small/public/2021-03/tm-mcdonalds-logo.png?itok=zWQPZ6Oo";
+
+img.classList = "img"
+        
+document.querySelector("#x").appendChild(img)
+
+
+
+        
+        
